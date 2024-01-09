53130bffd9046ab6cbb64603df63662283bf5f50
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2024-01-09 17:27:13 -0300

    PoC with Ocelot

--- Ocelot_PoC/ApiGateway/Program.cs
+++ Ocelot_PoC/ApiGateway/Program.cs
@@ -1,19 +1,33 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Application configuration to add the Ocelot .json file
+builder.Configuration.AddJsonFile("Ocelot/ocelot.json");
+
+// Container services
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseHsts();
+
+}
 
 app.UseHttpsRedirection();
-
-
+app.MapControllers();
+app.UseOcelot().Wait();
 app.Run();
-
